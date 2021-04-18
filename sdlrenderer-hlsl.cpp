86fc553a5eb5478337f12bda5afc7d334628165a
Felipe Tavares <felipe.oltavares@gmail.com>
2021-04-18 01:04:55 -0300

    Moved SDL_sysrender to before SDL.

--- sdlrenderer-hlsl.cpp
+++ sdlrenderer-hlsl.cpp
@@ -1,11 +1,12 @@
 #include <cassert>
 
+// This has the full definition for SDL_Renderer, which allows us to reference
+// private fields inside it.
+#include <SDL_sysrender.h>
+
 #include <SDL.h>
 #include <d3d9.h>
 
-// This has the full definition for SDL_Renderer, which allows us to reference
-// private fields inside it.
-#include <SDL_render.h>
 
 // These are from SDL_render_d3d.c (inside SDL)
 // We need to copy them here because they are not included by the header files.
